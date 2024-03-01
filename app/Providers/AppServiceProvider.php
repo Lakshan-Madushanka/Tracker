ec3bd88fa3bb107949d73be1831383d7bc9c6a2b
Lakshan Madushanka <epmadushanka@gmail.com>
2024-03-01 16:30:34 +0530

    Initiate

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::shouldBeStrict( ! $this->app->isProduction());
+    }
+}
