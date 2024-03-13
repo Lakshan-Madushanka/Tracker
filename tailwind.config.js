0e1712dea29b006564b069dafddde748f7ee8531
Lakshan Madushanka <epmadushanka@gmail.com>
2024-03-13 15:38:20 +0530

    Tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require("daisyui"),
+    ],
+}
+
