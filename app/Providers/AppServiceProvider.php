38918258a19f01e47bfd0bcb1d07f175af728741
Lakshan Madushanka <epmadushanka@gmail.com>
2024-03-13 18:09:47 +0530

    Overwrite empty string macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict( ! $this->app->isProduction());
+
+        $this->macros();
+    }
+
+    private function macros(): void
+    {
+        Str::macro('overwriteEmpty', function (mixed $string, string $replace = '---') {
+            if (is_string($string) && Str::squish($string) === '') {
+                return $replace;
+            }
+
+            if ( ! $string) {
+                return $replace;
+            }
+
+            return $string;
+        });
     }
 }
